1b1d10b8f835b2ffde6d91f659388f38fd901d79
Joshua_Rodrigues <joshua_rodrigues@aiap.sg>
2022-03-10 01:46:28 +0800

    Completed data kaggle downloader

---
+++ src/data_downloader.py
@@ -0,0 +1,39 @@
+
+import argparse
+import os
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+
+def create_folders(folder_list):
+    for i in folder_list:
+        if not os.path.exists(i):
+            os.makedirs(i)
+            print(f"folder is created: {i}")
+        else:
+            print(f"folder already exists: {i}")
+
+
+def dataset_downloader(datset):
+    api = KaggleApi()
+    api.authenticate()  
+    print(f"starting download of {dataset}")
+    api.dataset_download_files(datset, path='data/raw', unzip=True)
+
+
+
+if __name__ =="__main__":
+    DIR_RAW = "data/raw"
+    DIR_ClEAN = "data/clean"
+    folder= [DIR_RAW,DIR_ClEAN]
+    create_folders(folder)
+
+    parser = argparse.ArgumentParser(description='data_downloader')
+
+    parser.add_argument('--data_set',default="dgomonov/new-york-city-airbnb-open-data",
+                        help='Kaggle dataset download. defaults to "dgomonov/new-york-city-airbnb-open-data"' , type=str)
+
+    args = parser.parse_args()
+    params = vars(args)
+    dataset=params["data_set"]
+    
+    dataset_downloader(dataset)
